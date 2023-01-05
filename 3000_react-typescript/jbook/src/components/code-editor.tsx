229b7bf64b931122c0e77cca71ae392a96c56617
Pavit Pimchanagul <pavitpim@gmail.com>
2023-01-05 11:36:16 +0700

    section10-11:mocaco editor with config

---
+++ 3000_react-typescript/jbook/src/components/code-editor.tsx
@@ -0,0 +1,84 @@
+import './code-editor.css'
+import './syntax.css'
+import { useRef } from 'react';
+import MonacoEditor, { OnMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
+import MonacoJSXHighlighter from 'monaco-jsx-highlighter';
+import {parse} from '@babel/parser'
+import traverse from '@babel/traverse'
+
+
+
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const editorRef = useRef<any>();
+  const onEditorDidMount: OnMount = (editor) => {
+    editorRef.current = editor;
+    editor.onDidChangeModelContent(() => {
+      console.log(editor.getValue());
+      onChange(editor.getValue());
+    });
+
+    editor.getModel()?.updateOptions({ tabSize: 2 });
+    const highlighter = new MonacoJSXHighlighter(
+      // @ts-ignore
+      window.monaco,parse,traverse,editor)
+
+    // const highlighter = new MonacoJSXHighlighter(
+    //   // @ts-ignore
+    //   window.monaco,codeShift,editor)
+
+
+      highlighter.highLightOnDidChangeModelContent(
+        ()=>{},
+        ()=>{},
+        undefined,
+        ()=>{}
+      );
+  };
+
+  const onFormatClick = () => {
+    // console.log(editorRef.current)
+    const unformatted = editorRef.current.getModel().getValue();
+
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    }).replace(/\n$/,"");
+    console.log(formatted);
+    editorRef.current.setValue(formatted);
+  };
+  return (
+    <div className='editor-wrapper'>
+      <button className='button button-format is-primary is-small' onClick={onFormatClick}>
+        Format
+      </button>
+      <MonacoEditor
+        value={initialValue}
+        onMount={onEditorDidMount}
+        language='javascript'
+        height='500px'
+        theme='vs-dark'
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: true,
+          lineNumbersMinChars: 3,
+          fontSize: 14,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </div>
+  );
+};
+
+export default CodeEditor;
--- 3000_react-typescript/jbook/src/index.tsx
+++ 3000_react-typescript/jbook/src/index.tsx
@@ -1,59 +1,80 @@
-import * as esbuild from 'esbuild-wasm'
-import {useState,useEffect,useRef} from 'react'
-import ReactDOM from 'react-dom'
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
-import { fetchPlugin } from './plugins/fetch-plugin'
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
-const App =() => {
-  const esbuildRef = useRef<any>()
-  const [input,setInput] = useState('')
-  const [code,setCode] = useState('')
+
+const App = () => {
+  const esbuildRef = useRef<any>();
+  const iframeRef = useRef<any>();
+  const [input, setInput] = useState('');
 
   const startService = async () => {
-    esbuildRef.current =  await esbuild.startService({
-      worker:true,
-      wasmURL : 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-    })
-  }
+    esbuildRef.current = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+    });
+  };
 
-  useEffect(()=>{
-    startService()
-  },[])
+  useEffect(() => {
+    startService();
+   
+  }, []);
 
   const onClick = async () => {
-  if(!esbuildRef.current) return;
+    if (!esbuildRef.current) return;
 
-  const result = await esbuildRef.current.build({
-    entryPoints:['index.js'],
-    bundle:true,
-    write : false,
-    plugins : [
-      unpkgPathPlugin(),
-      fetchPlugin(input)
-    ],
-    define : {
-      'process.env.NODE_ENV': '"production"',
-      global: 'window'
-    }
-  })
+    iframeRef.current.srcdoc = html;
+    const result = await esbuildRef.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
 
-  setCode(result.outputFiles[0].text)
-  console.log(result)
-  }
+    iframeRef.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+  };
+
+  const html = `
+   <html>
+    <head></head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener('message', (event)=> {
+         try {
+          eval(event.data)
+         } catch (err){
+            const root = document.querySelector("#root");
+            root.innerHTML = '<div style="color:red;"><h4>Runtime Error</h4>'+err +'</div>'
+            // throw(err)
+            console.error(err)
+         }
+        },false)
+      </script>
+    </body>
+   </html>
+  `;
+
   return (
     <div>
-      <textarea 
-      value={input}
-      onChange={e=> setInput(e.target.value)} />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <pre>{code}</pre>
+      <CodeEditor 
+      initialValue='const a = 5;'
+      onChange={(value)=> setInput(value)}
+      />
+      <textarea value={input} onChange={(e) => setInput(e.target.value)} />
+      <div><button onClick={onClick}>Submit</button></div>
+
+      <iframe srcDoc={html} title='code preview' sandbox='allow-scripts' ref={iframeRef} />
     </div>
-  )
-}
+  );
+};
 
-
-ReactDOM.render(
-  <App/>,document.querySelector("#root")
-)
+ReactDOM.render(<App />, document.querySelector('#root'));
